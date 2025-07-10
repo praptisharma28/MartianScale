a98e8a373cc3009d63347b24c07870b0fdc8a0ab
Prapti Sharma <praptisharma282003@gmail.com>
2025-07-10 23:47:35 +0530

    borrowing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use std::io;
 
 fn main() {
     let mut input = String::new(); //string is a type of smart pointer in rust
-    let mut s = input;
+    // let mut s = input;
+
+    some_func(&mut input); // passing a reference to the function, so we don't transfer ownership
+
+
     println!("Enter your weight on Earth in kg: ");
     io::stdin().read_line(&mut input);
     let mars_weight = calculate_weight_on_mars(100.0);
@@ -16,3 +20,10 @@ fn main() {
 fn calculate_weight_on_mars(weight: f64) -> f64 {
     (weight / 9.81) * 3.711
 } 
+
+// to be able to pass varibles to functions without transferring ownership, we can use references
+// references allow us to borrow the value without taking ownership, we do that by adding an ampersand (&) before the variable name
+
+fn some_func(s: &mut String) {
+    println!("The value of s is: {}", s);
+}
