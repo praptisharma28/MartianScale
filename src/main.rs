b5c1e57092cdbed69ee31cd24b3232097cbcc4b7
Prapti Sharma <praptisharma282003@gmail.com>
2025-07-10 23:56:54 +0530

    references and borrowing

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,19 @@ fn main() {
     let mut input = String::new(); //string is a type of smart pointer in rust
     // let mut s = input;
 
-    some_func(&mut input); // passing a reference to the function, so we don't transfer ownership
+    //let s1 = &input; // s1 is a reference to input, it borrows the value of input
+    //let s2 = &input; // s2 is another reference to input, we can
+    //println!("{} and {}", s1, s2); // we can use both references to access the value of input
+    // if we have already referenced as mutable, we cannot reference it as immutable again
+    // but if we change it to mutable, then alsoit will result in an error, because we can have multipl immuable references, but only one mutable reference at a time
+    //but the compiler lets us do that, because it knows that it will be mutated later, so it will not allow us to use the immutable references
+
+    //some_func(&mut input); // passing a reference to the function, so we don't transfer ownership
 
 
     println!("Enter your weight on Earth in kg: ");
     io::stdin().read_line(&mut input);
+    // the read_line function mutably borrows the string and changes its content by filling it by input
     let mars_weight = calculate_weight_on_mars(100.0);
     println!("Weight on Mars: {} kg", mars_weight);
 }
@@ -24,6 +32,6 @@ fn calculate_weight_on_mars(weight: f64) -> f64 {
 // to be able to pass varibles to functions without transferring ownership, we can use references
 // references allow us to borrow the value without taking ownership, we do that by adding an ampersand (&) before the variable name
 
-fn some_func(s: &mut String) {
-    println!("The value of s is: {}", s);
-}
+// fn some_func(s: &mut String) {
+//     println!("The value of s is: {}", s);
+// }
