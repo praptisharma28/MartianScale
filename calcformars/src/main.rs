ed5e7f09b7692809d0c6a3868405eb5ec334a5cb
Prapti Sharma <praptisharma282003@gmail.com>
2025-07-10 17:29:44 +0530

    standard library

--- calcformars/src/main.rs
+++ calcformars/src/main.rs
@@ -1,6 +1,9 @@
+use std::io;
+
 fn main() {
-    let mut mars_weight = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1.5; // Adjusting for a hypothetical factor
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    let mars_weight = calculate_weight_on_mars(100.0);
     println!("Weight on Mars: {} kg", mars_weight);
 }
 
